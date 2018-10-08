8a20d25c24d48776293ce8020f2d97620a37fe21
Thayyeb salim <Thayyebsalim@gmail.com>
2018-10-08 10:34:20 +0400

    Made minor changes

--- Calculator.js
+++ Calculator.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-
   numPlace();
 
   //calculator "logic" starts
 
   var exp = "",
-    acc = 0,
-    val = "",
-    op = "",
-	curOp="";
+      acc = 0,
+      val = "",
+      op = "",
+      curOp = "";
 
-  $(".num").on('click', function() {
+  $(".num").on("click", function() {
     if (op === "=") {
       exp = "";
       op = "";
@@ -21,17 +20,18 @@ $(document).ready(function() {
     val += $(this).text();
     append();
   });
+
   //rep
-  $(".neg").on('click', function() {
-	 if(exp===""){
-		  exp += $(this).text();
-		  val += $(this).text();
-		  append();
-		  }
-		  if(/\d/.test(exp[exp.length - 1])){
-			  if (/\d/.test(exp[exp.length - 1])) {
-		curOp = $(this).text();
-		if (op !== "") {
+  $(".neg").on("click", function() {
+    if (exp === "") {
+      exp += $(this).text();
+      val += $(this).text();
+      append();
+    }
+    //repetition
+    if (/\d/.test(exp[exp.length - 1])) {
+      curOp = $(this).text();
+      if (op !== "") {
         switch (op) {
           case "+":
             acc += parseFloat(val);
@@ -40,7 +40,7 @@ $(document).ready(function() {
             acc *= parseFloat(val);
             break;
           case "-":
-				acc -= parseFloat(val);
+            acc -= parseFloat(val);
             break;
           case "/":
             acc /= parseFloat(val);
@@ -52,25 +52,20 @@ $(document).ready(function() {
       } else {
         acc += parseFloat(val);
       }
-      if (curOp === "=")
-	  {
-		  acc=Math.round(acc*1000)/1000; //To round digits
-		  exp = acc.toString();
-	  }
-	  else
-		  exp += curOp;
-    }
-    op = curOp;
-    val = "";      
-    append();
-			  }		  //repeatation
+      exp += curOp;
+      //repetition
+      op = curOp;
+      val = "";
+      append();
+    } //repeatation
   });
-  $(".op").on('click', function() {
-	  if(exp === "")
-		  return;
-	  if (/\d/.test(exp[exp.length - 1])) {
-		curOp = $(this).text();
-		if (op !== "") {
+
+  $(".op").on("click", function() {
+    if (exp === "") return;
+    //repetition
+    if (/\d/.test(exp[exp.length - 1])) {
+      curOp = $(this).text();
+      if (op !== "") {
         switch (op) {
           case "+":
             acc += parseFloat(val);
@@ -79,7 +74,7 @@ $(document).ready(function() {
             acc *= parseFloat(val);
             break;
           case "-":
-				acc -= parseFloat(val);
+            acc -= parseFloat(val);
             break;
           case "/":
             acc /= parseFloat(val);
@@ -91,38 +86,39 @@ $(document).ready(function() {
       } else {
         acc += parseFloat(val);
       }
-      if (curOp === "=")
-	  {
-		  acc=Math.round(acc*1000)/1000; //To round digits
-		  exp = acc.toString();
-	  }
-	  else
-		  exp += curOp;
-    }
-	if(curOp!=="-")val = "";
+      exp += curOp;
+    } //repetition
+
+    if (curOp !== "-") val = "";
     op = curOp;
     append();
   });
-  
+
+  $(".eq").on("click", function() {
+    acc = Math.round(acc * 1000) / 1000; //To round digits
+    exp = acc.toString();
+    append();
+    op = "=";
+  });
+
   //calculator "logic" ends
 
-  $(".ac").on('click', function() {
-	  val="";
-	  acc=0;
-	  exp=""
-	  append();
+  $(".ac").on("click", function() {
+    val = "";
+    acc = 0;
+    exp = "";
+    append();
   });
-  
-  $(".ce").on('click', function() {
-	  val="";
-	  append();
+
+  $(".ce").on("click", function() {
+    val = "";
+    append();
   });
-  
+
   function numPlace() {
     for (var i = 1; i < 10; i++) {
       $(".digs").append("<button class='num'>" + i + "</button>");
-      if (i % 3 == 0)
-        $(".digs").append("<br>");
+      if (i % 3 == 0) $(".digs").append("<br>");
     }
   } //to create number pads from 1 t0 9
 
@@ -131,5 +127,4 @@ $(document).ready(function() {
     $(".exp").append(exp);
     $(".ans").append(val);
   }
-
 });
